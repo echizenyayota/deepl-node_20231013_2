602fa477b39491c588718968198dd857ae231508
echizenyayota <virtualmisso30@gmail.com>
2023-10-13 22:28:24 +0900

    set the code to translate using deepl-node

--- server.js
+++ server.js
@@ -14,3 +14,11 @@ const app = express();
 app.use(cors());
 
 app.listen(PORT, () => console.log("Server running on PORT " + PORT)); 
+
+const authKey = process.env.DEEPL_API_KEY; // Replace with your key
+const translator = new deepl.Translator(authKey);
+
+(async () => {
+  const result = await translator.translateText('good evening', null, 'ja');
+  console.log(result.text); // こんにちは
+})(); 
