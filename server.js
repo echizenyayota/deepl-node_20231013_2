16cf7f1d3b01cb4b6000a23784dbbb1ce439fdb7
echizenyayota <virtualmisso30@gmail.com>
2023-10-15 15:02:04 +0900

    add codes to list available languages

--- server.js
+++ server.js
@@ -14,3 +14,13 @@ const app = express();
 app.use(cors());
 
 app.listen(PORT, () => console.log("Server running on PORT " + PORT)); 
+
+const authKey = process.env.DEEPL_API_KEY; // Replace with your key
+const translator = new deepl.Translator(authKey);
+
+const targetLanguages = await translator.getTargetLanguages();
+for (let i = 0; i < targetLanguages.length; i++) {
+    const lang = targetLanguages[i];
+    console.log(`${lang.name} (${lang.code})`);
+}
+
